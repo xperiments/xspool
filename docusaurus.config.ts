581c7e34ab021932a64ae8ffa0fb9d98e9da10b0
Pedro Casaubon <xperiments@gmail.com>
2025-03-21 23:03:23 +0100

    move to xspool.com

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   url: "https://xperiments.in/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/xspool/",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
